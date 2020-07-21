d0142608851682068bd2b8b9b03a487fe612ff90
louie.cho <louie@MacBook-Pro.local>
2020-07-21 16:19:51 +0800

    d

---
+++ precache-manifest.f226dcac7bfdea07940c89a4a495090c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45807f3e8a738120df519c260c2a4743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c4de0319ae34c37ef5b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07f8367c48d7e30aec35",
+    "url": "/static/js/2.b8a4510a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8a4510a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4de0319ae34c37ef5b",
+    "url": "/static/js/main.effb719d.chunk.js"
+  },
+  {
+    "revision": "51ea7638fc021bbefb02",
+    "url": "/static/js/runtime-main.f6001a2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
